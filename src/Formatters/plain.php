60a005df8a9381a73cd817a466dfcd2cb669fc6b
NickShelud <N.She.1997@mail.ru>
2022-12-25 15:37:58 +0300

    fix bugs

--- src/Formatters/plain.php
+++ src/Formatters/plain.php
@@ -7,36 +7,34 @@ use function Differ\Build\getValue;
 
 function getPlain(array $comparisonArray, string $oldKey = '')
 {
+
     return flatten(array_map(function ($item) use ($oldKey) {
         $status = $item['status'];
-        $key = $item['key'];
 
         if (isset($oldKey) and $oldKey != '') {
-            $key = "{$oldKey}.{$key}";
-        }
-
-        if (isset($item['value'])) {
-            if (is_array($item['value']) and $status != 'root') {
-                $item['value'] = "[complex value]";
-            } else {
-                $item['value'] = getValue($item['value']);
-            }
+            $key = "{$oldKey}.{$item['key']}";
+        } else {
+            $key = $item['key'];
         }
 
         switch ($status) {
             case 'add':
-                return "Property '{$key}' was added with value: {$item['value']}";
+                $value = getValue($item['value']);
+                return "Property '{$key}' was added with value: {$value}";
                 break;
             case 'remove':
                 return "Property '{$key}' was removed";
                 break;
             case 'update':
-                $oldValue = gettype($item['oldValue']) != 'array' ? getValue($item['oldValue']) : "[complex value]";
-                $item['value'] = is_null($item['value']) ? 'null' : $item['value'];
-                return "Property '{$key}' was updated. From {$item['value']} to {$oldValue}";
+                $value = getValue($item['value']);
+                $oldValue = getValue($item['oldValue']);
+                return "Property '{$key}' was updated. From {$value} to {$oldValue}";
                 break;
             case 'root':
                 return getPlain($item['children'], $key);
+                break;
+            case 'no change':
+                return [];
         }
     }, $comparisonArray));
 
@@ -45,7 +43,7 @@ function getPlain(array $comparisonArray, string $oldKey = '')
 
 function getPlainDiffFormat(array $array)
 {
-    $encode = json_encode($array, JSON_PRETTY_PRINT, JSON_PRESERVE_ZERO_FRACTION);
+    $encode = json_encode($array, JSON_PRETTY_PRINT);
     $char = ['"', ',', '    '];
     $trim = trim(str_replace($char, '', $encode), '[]');
 
