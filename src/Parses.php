a19dd13ab6d52a401c1ac13e9b01fc842199a265
NickShelud <N.She.1997@mail.ru>
2022-12-25 17:18:35 +0300

    fix bugs

--- src/Parses.php
+++ src/Parses.php
@@ -11,7 +11,7 @@ function pathToArray(string $path)
     $formatFile = pathinfo($format, PATHINFO_EXTENSION);
     if ($formatFile === 'json') {
         $contentFile = file_get_contents($path);
-        return json_decode($contentFile, true);
+        return json_decode($contentFile, true, JSON_THROW_ON_ERROR);
     } elseif ($formatFile === 'yml' or $formatFile === 'yaml') {
         return Yaml::parseFile($path);
     }
