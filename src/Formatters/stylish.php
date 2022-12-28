92091e4e8deb8cb2d299bcee52953ca4a58c4308
NickShelud <N.She.1997@mail.ru>
2022-12-28 18:24:13 +0300

    fix bugs

--- src/Formatters/stylish.php
+++ src/Formatters/stylish.php
@@ -2,60 +2,85 @@
 
 namespace DiffCalc\Formatter\Stylish;
 
+use function Differ\Build\getValue;
+
 function getStylish(array $comparisonArray)
 {
     //var_dump($item['children']);
-    return array_map(function ($item) {
-        //$key = $item['key'];
+    return array_reduce($comparisonArray, function ($acc, $item) {
+        $key = $item['key'];
         $status = $item['status'];
+        $depth = '  ';
 
-        if (isset($item['value']) and is_array($item['value'])) {
-            //$item['value'] = getStylish($item['value']);
-            $item['value'] = implode("\n", $item['value']);
-        }
-
-        switch ($status) {
-            case 'no change':
-                return "{$item['key']}: {$item['value']}";
-            case 'update':
-                $first = "- {$item['key']}: {$item['value']}";
-                $second = "+ {$item['key']}: {$item['oldValue']}";
-                return "{$first}\n{$second}";
-            case 'remove':
-                return "- {$item['key']}: {$item['value']}";
-            case 'add':
-                return "+ {$item['key']}: {$item['value']}";
-            case 'root':
-                //$children = getStylish($item['children']);
-                $strChildren = implode($item['children']);
-                //print_r($strChildren);
-                return "{$item['key']}: {$strChildren}";
-        }
-
-       //if ($status === 'no change') {
-       //    $acc[$key] = $item['value'];
-       //} elseif ($status === 'update') {
-       //    $acc['- ' . $key] = $item['value'];
-       //    $acc['+ ' . $key] = $item['oldValue'];
-       //} elseif ($status === 'remove') {
-       //    $acc['- ' . $key] = $item['value'];
-       //} elseif ($status === 'add') {
-       //    $acc['+ ' . $key] = $item['value'];
-       //} elseif ($status === 'root') {
-       //    $acc[$key] = getStylish($item['children']);
+       //if (isset($item['value']) and is_array($item['value'])) {
+       //    $newDepth = str_repeat($depth, 4);
+       //    $item['value'] = getStr($item['value'], $newDepth);
+       //} elseif (isset($item['children'])) {
+       //    $children = getStylish($item['children']);
+       //    $childrenStr = implode("\n", $children);
+       //} else {
+       //    $item['value'] = getValue($item['value']);
        //}
 
-        
-    }, $comparisonArray);
+       //if ($status === 'no change') {
+       //    $newDepth = str_repeat($depth, 4);
+       //    return "$newDepth{$key}: {$item['value']}";
+       //} elseif ($status === 'update') {
+       //    $newDepth = str_repeat($depth, 3);
+       //    return "$newDepth- {$key}: {$item['value']}\n$newDepth+ {$key}: {$item['value']}";
+       //} elseif ($status === 'remove') {
+       //    $newDepth = str_repeat($depth, 3);
+       //    return "$newDepth- {$key}: {$item['value']}";
+       //} elseif ($status === 'add') {
+       //    $newDepth = str_repeat($depth, 3);
+       //    return "$newDepth+ {$key}: {$item['value']}";
+       //} elseif ($status === 'root') {
+       //    $newDepth = str_repeat($depth, 2);
+       //    return "{$key}:\n {$childrenStr}";
+       //}
+
+
+        if ($status === 'no change') {
+            $acc[$key] = $item['value'];
+            return $acc;
+        } elseif ($status === 'update') {
+            $acc['- ' . $key] = $item['value'];
+            $acc['+ ' . $key] = $item['oldValue'];
+            return $acc;
+        } elseif ($status === 'remove') {
+            $acc['- ' . $key] = $item['value'];
+            return $acc;
+        } elseif ($status === 'add') {
+            $acc['+ ' . $key] = $item['value'];
+            return $acc;
+        } elseif ($status === 'root') {
+            $acc[$key] = getStylish($item['children']);
+            return $acc;
+        }
+    }, []);
+
+    return $result;
 }
 
 
 function getStylishDiffFormat(array $comparisonArray)
 {
+    //print_r($comparisonArray);
     //return $comparisonArray;
+    //$result = implode("\n", $comparisonArray);
+    //return $result;
     $encodeContent = json_encode($comparisonArray, JSON_PRETTY_PRINT);
     $char = ['"', ','];
     $formattedContent = str_replace($char, '', $encodeContent);
 
     return str_replace("  - ", "- ", str_replace("  + ", "+ ", $formattedContent));
+}
+
+function getStr($arr, $depth)
+{
+    $str = json_encode($arr, JSON_PRETTY_PRINT);
+    //print_r($str);
+    $str = ' ' . $str;
+    $char = ['"', ',', " {\n"];
+    return "{\n" . $depth . str_replace($char, '', $str);
 }
