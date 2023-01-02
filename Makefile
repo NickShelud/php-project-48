4c295ae7ab53190d202d1cb4fd3b8345ebade368
NickShelud <N.She.1997@mail.ru>
2023-01-02 18:21:21 +0300

    fix bugs

--- Makefile
+++ Makefile
@@ -3,7 +3,6 @@ install:
 
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin tests
-
 test:
 	composer exec --verbose phpunit tests
 
--- src/Formatters/stylish.php
+++ src/Formatters/stylish.php
@@ -4,83 +4,99 @@ namespace DiffCalc\Formatter\Stylish;
 
 use function Differ\Build\getValue;
 
-function getStylish(array $comparisonArray)
+function getStylish(array $comparisonArray, int $depth = 1)
 {
-    //var_dump($item['children']);
-    return array_reduce($comparisonArray, function ($acc, $item) {
-        $key = $item['key'];
-        $status = $item['status'];
-        $depth = '  ';
 
-       //if (isset($item['value']) and is_array($item['value'])) {
-       //    $newDepth = str_repeat($depth, 4);
-       //    $item['value'] = getStr($item['value'], $newDepth);
-       //} elseif (isset($item['children'])) {
-       //    $children = getStylish($item['children']);
-       //    $childrenStr = implode("\n", $children);
-       //} else {
-       //    $item['value'] = getValue($item['value']);
-       //}
+    return array_map(function ($item) use ($depth) {
+        $key = $item['key'] ?? null;
+        $status = $item['status'] ?? null;
 
-       //if ($status === 'no change') {
-       //    $newDepth = str_repeat($depth, 4);
-       //    return "$newDepth{$key}: {$item['value']}";
-       //} elseif ($status === 'update') {
-       //    $newDepth = str_repeat($depth, 3);
-       //    return "$newDepth- {$key}: {$item['value']}\n$newDepth+ {$key}: {$item['value']}";
-       //} elseif ($status === 'remove') {
-       //    $newDepth = str_repeat($depth, 3);
-       //    return "$newDepth- {$key}: {$item['value']}";
-       //} elseif ($status === 'add') {
-       //    $newDepth = str_repeat($depth, 3);
-       //    return "$newDepth+ {$key}: {$item['value']}";
-       //} elseif ($status === 'root') {
-       //    $newDepth = str_repeat($depth, 2);
-       //    return "{$key}:\n {$childrenStr}";
-       //}
-
+        $indent = getIndent($depth);
+        $smallIndent = getSmallIndent($depth);
 
         if ($status === 'no change') {
-            $acc[$key] = $item['value'];
-            return $acc;
+            $value = getArrayToStr($item['value'], $depth);
+            return "{$indent}{$key}: {$value}";
         } elseif ($status === 'update') {
-            $acc['- ' . $key] = $item['value'];
-            $acc['+ ' . $key] = $item['oldValue'];
-            return $acc;
-        } elseif ($status === 'remove') {
-            $acc['- ' . $key] = $item['value'];
-            return $acc;
-        } elseif ($status === 'add') {
-            $acc['+ ' . $key] = $item['value'];
-            return $acc;
-        } elseif ($status === 'root') {
-            $acc[$key] = getStylish($item['children']);
-            return $acc;
-        }
-    }, []);
+            $value = getArrayToStr($item['value'], $depth);
+            $oldValue = getArrayToStr($item['oldValue'], $depth);
 
-    return $result;
+            if (is_array($item['value'])) {
+                return "{$smallIndent}- {$key}: {{$value}\n$smallIndent  }\n$smallIndent+ {$key}: {$oldValue}";
+            } elseif (is_array($item['oldValue'])) {
+                return "{$smallIndent}- {$key}: {$value}\n$smallIndent+ {$key}: {{$oldValue}\n{$smallIndent}}";
+            }
+
+            return "{$smallIndent}- {$key}: {$value}\n$smallIndent+ {$key}: {$oldValue}";
+        } elseif ($status === 'remove') {
+            $value = getArrayToStr($item['value'], $depth);
+
+            if (is_array($item['value'])) {
+                return "{$smallIndent}- {$key}: {{$value}\n  {$smallIndent}}";
+            }
+            return "{$smallIndent}- {$key}: {$value}";
+        } elseif ($status === 'add') {
+            $value = getArrayToStr($item['value'], $depth);
+            if (is_array($item['value'])) {
+                return "{$smallIndent}+ $key: {" . "$value\n  {$smallIndent}}";
+            }
+            return "{$smallIndent}+ $key: {$value}";
+        } elseif ($status === 'root') {
+            $children = implode("\n", getStylish($item['children'], $depth + 1));
+            return "{$indent}{$key}: {\n" . $children . "\n{$indent}}";
+        }
+    }, $comparisonArray);
 }
 
 
 function getStylishDiffFormat(array $comparisonArray)
 {
-    //print_r($comparisonArray);
-    //return $comparisonArray;
-    //$result = implode("\n", $comparisonArray);
-    //return $result;
-    $encodeContent = json_encode($comparisonArray, JSON_PRETTY_PRINT);
-    $char = ['"', ','];
-    $formattedContent = str_replace($char, '', $encodeContent);
-
-    return str_replace("  - ", "- ", str_replace("  + ", "+ ", $formattedContent));
+    $result = implode("\n", $comparisonArray);
+    return "{\n" . $result . "\n}";
 }
 
-function getStr($arr, $depth)
+function getString(mixed $value): string
 {
-    $str = json_encode($arr, JSON_PRETTY_PRINT);
-    //print_r($str);
-    $str = ' ' . $str;
-    $char = ['"', ',', " {\n"];
-    return "{\n" . $depth . str_replace($char, '', $str);
+    if (is_bool($value)) {
+        return $value ? 'true' : 'false';
+    } elseif (is_null($value)) {
+        return 'null';
+    } elseif (is_string($value)) {
+        return $value;
+    } elseif (is_int($value)) {
+        return "{$value}";
+    }
+}
+
+function getIndent(int $depth = 1, int $depthForSmallIndent = 0)
+{
+    $indent = 4;
+
+    return str_repeat(' ', $indent * $depth - $depthForSmallIndent);
+}
+
+function getSmallIndent(int $depth)
+{
+    return getIndent($depth, 2);
+}
+
+function getArrayToStr(mixed $value, int $depth): string
+{
+    if (!is_array($value)) {
+        return getString($value);
+    }
+    $key = key($value);
+    $keys = array_keys($value);
+    $result = array_map(function ($item) use ($key, $depth, $keys) {
+        $indent = getIndent($depth + 1);
+        if (is_array($item)) {
+            return "\n{$indent}{$key}: {" . getArrayToStr($item, $depth + 1) . "\n{$indent}}";
+        } else {
+            $key = $keys[count($keys) - 1];
+            return "\n{$indent}$key: $item";
+        }
+
+        //print_r($result);
+    }, $value);
+    return str_replace("\n\n", "\n", implode("\n", $result));
 }
