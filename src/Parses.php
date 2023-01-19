bf8c5b75fd88e69a0dd41b52cbac894470e7f2c3
NickShelud <N.She.1997@mail.ru>
2023-01-19 08:46:02 +0300

    fix bugs

--- src/Parses.php
+++ src/Parses.php
@@ -7,7 +7,7 @@ use Symfony\Component\Yaml\Yaml;
 function pathToArray(string $path, string $format)
 {
     if ($format === 'json') {
-        $file = file_get_contents($path);
+        (string) $file = file_get_contents($path);
         return json_decode($file, true);
     } elseif ($format === 'yml' or $format === 'yaml') {
         return Yaml::parseFile($path);
